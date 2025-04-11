5209a5c64b9d4edcd09e6c55ce2035f9be0e59b4
Sam Weiss <sweiss@cct.ie>
2025-04-11 17:10:38 +0100

    Font size change

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 let submitColourButton = document.getElementById("submit-bg-colour-button");
 let colourSelector = document.getElementById("background-colour-selector");
 let body = document.getElementsByTagName("body")[0];
+let submitFontSizeButton = document.getElementById("submit-font-size-button");
+let fontSizeSelector = document.getElementById("font-size-selector");
 
 body.style.backgroundColor = localStorage.getItem("background-colour");
+body.style.fontSize = localStorage.getItem("font-size");
 
 submitColourButton.addEventListener("click", () => {
     let newColour = colourSelector.value;
     body.style.backgroundColor = newColour;
 
     localStorage.setItem("background-colour", newColour);
+});
+
+/*
+    Exercise
+    Include an input that will change the text size on the screen
+    Persist this setting
+*/
+
+submitFontSizeButton.addEventListener("click", () => {
+    let newFontSize = fontSizeSelector.value+"px";
+    body.style.fontSize = newFontSize;
+
+    localStorage.setItem("font-size", newFontSize);
 })
